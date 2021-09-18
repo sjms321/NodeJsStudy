4f1d982efda1d82cd98d9fd3ca250dcd828ed948
sjms321 <sjms321@naver.com>
2021-09-18 17:07:09 +0900

    start nodejs and be surprised by the power and simplicity of nodejs

---
+++ use_nodeMailer.js
@@ -0,0 +1,29 @@
+const nodemailer =require('nodemailer');
+const email = {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+    user: "259a36d385bb59",
+    pass: "fc915f72809742"
+  }
+}
+
+const send = async (option) => {
+    nodemailer.createTransport(email).sendMail(option,(error,info) => {
+        if(error){
+        }else{
+            console.log(info);
+            return info.response;
+        }
+    })
+}
+
+
+let email_data ={
+    from:"rlaalstjr831@gmail.com",
+    to: "sjms321@naver.com",
+    subject:"쉽고 강한 NodeJs",
+    text:"커뮤니티 활성화의 중요성"
+}
+
+send(email_data)
